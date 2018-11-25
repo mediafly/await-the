a22f6442fd4c8905ac1139365f84ee4caca0af75
Marcus Motill <marcus.motill@nexd.io>
2018-11-25 17:26:01 -0600

    fix limiter tests

--- test/Limiter.js
+++ test/Limiter.js
@@ -7,166 +7,196 @@ const assert = require('assert');
 describe('Limiter test', function() {
     this.timeout(30000);
 
-    it('should return if no collection is supplied', done => {
-        const limiter = new the.Limiter();
+    it('should return if no collection is supplied', async () => {
+        return new Promise((resolve, reject) => {
+            const limiter = new the.Limiter();
 
-        let iterations = 0;
-        limiter.on('iteration', () => {
-            iterations++;
-        });
+            let iterations = 0;
+            limiter.on('iteration', () => {
+                iterations++;
+            });
 
-        limiter.on('done', () => {
-            assert.strictEqual(iterations, 0);
-            return done();
-        });
-
-        limiter.on('error', ({ error }) => {
-            assert(!error, `Got error ${error.message}`);
-            return done();
-        });
-
-        limiter.start();
-    });
-
-    it('should do iterate 2 times and call done', done => {
-        const collection = ['item1', 'item2'];
-        const limiter = new the.Limiter(collection);
-
-        let iterations = 0;
-        limiter.on('iteration', () => {
-            iterations++;
-        });
-
-        limiter.on('done', () => {
-            assert.strictEqual(iterations, 2);
-            return done();
-        });
-
-        limiter.on('error', ({ error }) => {
-            assert(!error, `Got error ${error.message}`);
-            return done();
-        });
-
-        limiter.start();
-    });
-
-    it('should be able to run serially with a limit of 1', done => {
-        const collection = ['waiter', 'check please'];
-        const limiter = new the.Limiter(
-            collection,
-            async (value, key) => {
-                if (key === 0) {
-                    await the.wait(1000);
+            limiter.on('done', () => {
+                try {
+                    assert.strictEqual(iterations, 0);
+                } catch (e) {
+                    return reject(e);
                 }
+                return resolve();
+            });
 
-                return value;
-            },
-            { limit: 1 }
-        );
+            limiter.on('error', ({ error }) => {
+                return reject(`Got error ${error.message}`);
+            });
 
-        let results = [];
-        limiter.on('iteration', ({ resultValue }) => {
-            results.push(resultValue);
+            limiter.start();
         });
-
-        limiter.on('done', () => {
-            assert.strictEqual(_.size(results), 2);
-            assert.strictEqual(_.first(results), 'waiter');
-            assert.strictEqual(_.last(results), 'check please');
-            return done();
-        });
-
-        limiter.on('error', ({ error }) => {
-            assert(!error, `Got error ${error.message}`);
-            return done();
-        });
-
-        limiter.start();
     });
 
-    it('should be able to run in parallel with a limit of 3', done => {
-        const collection = ['waiter1', 'waiter2', 'check please'];
+    it('should do iterate 2 times and call done', async () => {
+        return new Promise((resolve, reject) => {
+            const collection = ['item1', 'item2'];
+            const limiter = new the.Limiter(collection);
 
-        const limiter = new the.Limiter(
-            collection,
-            async (value, key) => {
-                if (key === 0) {
-                    await the.wait(1000);
-                } else if (key === 1) {
-                    await the.wait(1000);
+            let iterations = 0;
+            limiter.on('iteration', () => {
+                iterations++;
+            });
+
+            limiter.on('done', () => {
+                try {
+                    assert.strictEqual(iterations, 2);
+                } catch (e) {
+                    return reject(e);
                 }
+                return resolve();
+            });
 
-                return value;
-            },
-            { limit: 3 }
-        );
+            limiter.on('error', ({ error }) => {
+                return reject(`Got error ${error.message}`);
+            });
 
-        let results = [];
-        limiter.on('iteration', ({ resultValue }) => {
-            results.push(resultValue);
+            limiter.start();
         });
+    });
 
-        let timer;
-        limiter.on('done', () => {
-            const totalTime = Date.now() - timer;
-            assert.strictEqual(_.size(results), 3);
-            assert(
-                totalTime < 2000,
-                `Expected execution to take less than 2 seconds to prove parallelism got ${totalTime}`
+    it('should be able to run serially with a limit of 1', async () => {
+        return new Promise((resolve, reject) => {
+            const collection = ['waiter', 'check please'];
+            const limiter = new the.Limiter(
+                collection,
+                async (value, key) => {
+                    if (key === 0) {
+                        await the.wait(1000);
+                    }
+
+                    return value;
+                },
+                { limit: 1 }
             );
-            assert.strictEqual(_.first(results), 'check please');
-            return done();
-        });
 
-        limiter.on('error', ({ error }) => {
-            assert(!error, `Got error ${error.message}`);
-            return done();
-        });
+            let results = [];
+            limiter.on('iteration', ({ resultValue }) => {
+                results.push(resultValue);
+            });
 
-        timer = Date.now();
-        limiter.start();
-    });
-
-    it('should be able to run in parallel with a limit of 2', done => {
-        const collection = ['waiter1', 'waiter2', 'check please'];
-
-        const limiter = new the.Limiter(
-            collection,
-            async (value, key) => {
-                if (key === 0) {
-                    await the.wait(2000);
-                } else if (key === 1) {
-                    await the.wait(1000);
+            limiter.on('done', () => {
+                try {
+                    assert.strictEqual(_.size(results), 2);
+                    assert.strictEqual(_.first(results), 'waiter');
+                    assert.strictEqual(_.last(results), 'check please');
+                } catch (e) {
+                    return reject(e);
                 }
 
-                return value;
-            },
-            { limit: 2 }
-        );
+                return resolve();
+            });
 
-        let results = [];
-        limiter.on('iteration', ({ resultValue }) => {
-            results.push(resultValue);
+            limiter.on('error', ({ error }) => {
+                return reject(`Got error ${error.message}`);
+            });
+
+            limiter.start();
         });
+    });
 
-        let timer;
-        limiter.on('done', () => {
-            const totalTime = Date.now() - timer;
-            assert.strictEqual(_.size(results), 3);
-            assert(totalTime < 3000, 'Expected execution to take less than 3 seconds to prove parallelism');
-            assert.strictEqual(results[0], 'waiter2');
-            assert.strictEqual(results[1], 'check please');
-            assert.strictEqual(results[2], 'waiter1');
-            return done();
+    it('should be able to run in parallel with a limit of 3', async () => {
+        return new Promise((resolve, reject) => {
+            const collection = ['waiter1', 'waiter2', 'check please'];
+
+            const limiter = new the.Limiter(
+                collection,
+                async (value, key) => {
+                    if (key === 0) {
+                        await the.wait(1000);
+                    } else if (key === 1) {
+                        await the.wait(1000);
+                    }
+
+                    return value;
+                },
+                { limit: 3 }
+            );
+
+            let results = [];
+            limiter.on('iteration', ({ resultValue }) => {
+                results.push(resultValue);
+            });
+
+            let timer;
+            limiter.on('done', () => {
+                const totalTime = Date.now() - timer;
+                try {
+                    assert.strictEqual(_.size(results), 3);
+                    assert(
+                        totalTime < 2000,
+                        `Expected execution to take less than 2 seconds to prove parallelism got ${totalTime}`
+                    );
+                    assert.strictEqual(_.first(results), 'check please');
+                } catch (e) {
+                    return reject(e);
+                }
+                return resolve();
+            });
+
+            limiter.on('error', ({ error }) => {
+                return reject(`Got error ${error.message}`);
+            });
+
+            timer = Date.now();
+            limiter.start();
         });
+    });
 
-        limiter.on('error', ({ error }) => {
-            assert(!error, `Got error ${error.message}`);
-            return done();
+    it('should be able to run in parallel with a limit of 2', async () => {
+        return new Promise((resolve, reject) => {
+            const collection = ['waiter1', 'waiter2', 'check please'];
+
+            const limiter = new the.Limiter(
+                collection,
+                async (value, key) => {
+                    if (key === 0) {
+                        await the.wait(2000);
+                    } else if (key === 1) {
+                        await the.wait(1000);
+                    }
+
+                    return value;
+                },
+                { limit: 2 }
+            );
+
+            let results = [];
+            limiter.on('iteration', ({ resultValue }) => {
+                results.push(resultValue);
+            });
+
+            let timer;
+            limiter.on('done', () => {
+                const totalTime = Date.now() - timer;
+                try {
+                    assert.strictEqual(_.size(results), 3);
+                    assert(
+                        totalTime < 3000,
+                        'Expected execution to take less than 3 seconds to prove parallelism'
+                    );
+                    assert.strictEqual(results[0], 'waiter2');
+                    assert.strictEqual(results[1], 'check please');
+                    assert.strictEqual(results[2], 'waiter1');
+                } catch (e) {
+                    return reject(e);
+                }
+
+                return resolve();
+            });
+
+            limiter.on('error', ({ error }) => {
+                return reject(`got error ${error}`);
+            });
+
+            timer = Date.now();
+            limiter.start();
         });
-
-        timer = Date.now();
-        limiter.start();
     });
 
     it('should throw for invalid channel', () => {
