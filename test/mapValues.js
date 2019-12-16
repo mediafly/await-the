764ac4afa44b0a4d921fd42b7808f6d637e82799
Scott Gress <scott.gress@olono.ai>
2019-12-16 11:44:08 -0600

    Retain key order in _.mapValues

--- test/mapValues.js
+++ test/mapValues.js
@@ -64,47 +64,59 @@ describe('Map Values test', function() {
         assert.deepStrictEqual(output, { 0: 'item10', 1: 'item21', 2: 'item32' });
     });
 
-    it('should run in parallel if the limit greater than 1', async () => {
+    it('should run in parallel if the limit greater than 1, and retain key order', async () => {
         const collection = {
             'item1.dummy': 'item-1',
             item2: 'item-2',
             item3: 'item-3'
         };
         const task = async (value, key) => {
-            await the.wait(500);
+            const delay = value === 'item-1' ? 500 : value === 'item-2' ? 300 : 150;
+            await the.wait(delay);
             return `${value}${key}`;
         };
 
         const start = Date.now();
-        const output = await the.mapValues(collection, task, { limit: 2 });
+        const output = await the.mapValues(collection, task, { limit: 3 });
         const duration = Date.now() - start;
-        assert(duration < 1500, 'Expected promises to run in parallel');
+        assert(duration < 550, 'Expected promises to run in parallel');
         assert.deepStrictEqual(output, {
             'item1.dummy': 'item-1item1.dummy',
             item2: 'item-2item2',
             item3: 'item-3item3'
         });
+        assert.deepEqual(
+            Object.keys(output),
+            ['item1.dummy', 'item2', 'item3'],
+            'Expected key order to be maintained'
+        );
     });
 
-    it('should run in parallel if the concurrency greater than 1', async () => {
+    it('should run in parallel if the concurrency greater than 1, and retain key order', async () => {
         const collection = {
             item1: 'item-1',
             item2: 'item-2',
             item3: 'item-3'
         };
         const task = async (value, key) => {
-            await the.wait(500);
+            const delay = value === 'item1' ? 500 : value === 'item2' ? 300 : 150;
+            await the.wait(delay);
             return `${value}${key}`;
         };
 
         const start = Date.now();
-        const output = await the.mapValues(collection, task, { concurrency: 2 });
+        const output = await the.mapValues(collection, task, { concurrency: 3 });
         const duration = Date.now() - start;
-        assert(duration < 1500, 'Expected promises to run in parallel');
+        assert(duration < 500, 'Expected promises to run in parallel');
         assert.deepStrictEqual(output, {
             item1: 'item-1item1',
             item2: 'item-2item2',
             item3: 'item-3item3'
         });
+        assert.deepEqual(
+            Object.keys(output),
+            ['item1', 'item2', 'item3'],
+            'Expected key order to be maintained'
+        );
     });
 });
